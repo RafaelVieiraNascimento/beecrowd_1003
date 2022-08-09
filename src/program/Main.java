eb12eb611fcc0cd2d59b579fe5a3f57a8a58ba51
RafaelVN <contato@RafaelVN.com>
2022-08-09 13:28:26 -0300

    beecrowd_1003

---
+++ src/program/Main.java
@@ -0,0 +1,32 @@
+package program;
+
+/*
+beecrowd | 1003 - Soma Simples
+
+Leia dois valores inteiros, no caso para variáveis A e B.
+A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco
+antes e depois da igualdade seguido pelo valor correspondente à soma de A e B.
+Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado,
+caso contrário, você receberá "Presentation Error".
+*/
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println("SOMA = " + (A+B));
+		sc.close();
+	}
+}
